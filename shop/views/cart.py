e9ca4715a0b2b7ee4626c1bde32ec8492aad589a
Mohammad Rahimi <moyi.pary@gmail.com>
2021-12-23 20:57:22 +0330

    I deleted the data base... Edit the models...
    1- Category
    2- Product
    3- User
    4- Cart
    
    Edit the detail template like that:
      add a form for adding a product to the cart

--- shop/views/cart.py
+++ shop/views/cart.py
@@ -2,5 +2,6 @@ from .imports import *
 
 
 def cart(request, p_id):
-    get_object_or_404(Product, id=p_id)
+    selected_product = get_object_or_404(Product, id=p_id)
+
     return HttpResponse("hello!")
