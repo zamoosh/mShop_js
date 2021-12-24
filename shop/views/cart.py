b5abc352e46c426cbc74c51a2bf51b2625e0b289
Mohammad Rahimi <moyi.pary@gmail.com>
2021-12-25 01:42:17 +0330

    version 1
    Send data using form to cart.py...
    
    1- edit cart.py to get the date from details.html
    
    2- in details.html I add two inputs:
        one for submit date
        and one wich is hidden for send the product.id to cart.py
    
    3- add some object to the database...
    
    4- edit the cart model to have:
      ManyToMany rel to product
      ForeignKey rel to user
    
    5- the code that syncs the database with the project is:
      "python manage.py migrate --run-syncdb"

--- shop/views/cart.py
+++ shop/views/cart.py
@@ -2,6 +2,12 @@ from .imports import *
 
 
 def cart(request, p_id):
-    selected_product = get_object_or_404(Product, id=p_id)
+    selected_product = get_object_or_404(Product, id=request.POST['cart'])
+    try:
+        if request.POST["add to cart"]:
+            print("salam")
+    except (Exception, TypeError):
+        print("na shod!")
+        print(p_id)
 
-    return HttpResponse("hello!")
+    return HttpResponse(selected_product)
