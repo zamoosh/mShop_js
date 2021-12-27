f09721ada5e02a8c56a67aa7b45a53dd7c8c4d65
Mohammad Rahimi <moyi.pary@gmail.com>
2021-12-27 17:46:57 +0330

    Now we can add products to our cart (varsion 2)
    1- add a botton to go back to main menu in /templates/shop/cart.html
    2- make whole the site dark
    3- update the cart.html to show the whole product in cart
    4- in /shop/views/cart.py add some code to insert products in cookies and
    	use them to add products to the cart that belongs to AnonymousUser
    5- register the cart in /shop/admin.py
    
    6- برای اینکه رفرش چیزی به کارت اضافه نکنه، با علیرضا فکر کردیم
    	ولی تنها راه موجود استفاده از جاوا اسکریپت بود که افتخاری گفت...

--- shop/admin.py
+++ shop/admin.py
@@ -26,8 +26,16 @@ class ProductAdmin(admin.ModelAdmin):
     list_per_page = 4
 
 
+class ShoppingCartAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("cart user", {"fields": ["user"]}),
+        ("cart products", {"fields": ["product"]}),
+    ]
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
 admin.site.site_header = 'Main menu'
 admin.site.site_title = 'Admin panel for Shop'
 admin.site.index_title = 'admin panel'
--- shop/views/cart.py
+++ shop/views/cart.py
@@ -3,11 +3,26 @@ from .imports import *
 
 def cart(request, p_id):
     selected_product = get_object_or_404(Product, id=request.POST['cart'])
-    try:
-        if request.POST["add to cart"]:
-            print("salam")
-    except (Exception, TypeError):
-        print("na shod!")
-        print(p_id)
-
-    return HttpResponse(selected_product)
+    if "product" in request.COOKIES:
+        cookie = request.COOKIES["product"] + "_"
+        cookie += selected_product.name
+        products = []
+        cookies = cookie.split("_")
+        for item in cookies:
+            products.append(Product.objects.get(name=item))
+        context = {
+            "products": products
+        }
+        # order of row 21 and 22 is matter and cookies must set at end...
+        response = render(request, "shop/cart.html", context)
+        response.set_cookie("product", cookie)
+        return response
+    else:
+        # setting the cookies
+        context = {
+            "user": request.user,
+            "product": selected_product
+        }
+        response = render(request, "shop/cart.html", context)
+        response.set_cookie("product", selected_product)
+        return response
